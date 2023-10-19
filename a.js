e9b4cc7edfeddcb0f30f9b14548a5c7d3fc7b354
Md. Seaum Ibne Mamun <94161634+SeaumIbneMamun@users.noreply.github.com>
2023-10-19 10:07:14 +0600

    Add files via upload

---
+++ a.js
@@ -0,0 +1,242 @@
+var price = 19;
+
+var address = 'Sabujbagh';
+
+var IsWeatherSunny = true;
+
+
+console.log(price , address , IsWeatherSunny)
+
+
+
+
+var string = 'word';
+console.log(typeof(string));
+
+var string1 = '232434';
+console.log(typeof(string1));
+
+var number = 212312;
+console.log(typeof(number));
+
+var isThisAssignment = true;
+console.log(typeof(isThisAssignment));
+
+
+
+
+var first = 'Seaum';
+
+var last = 'Mamun';
+
+var name = first+last;
+
+console.log(name);
+
+
+
+
+
+var first1 = 'Seaum';
+
+var last1 = 'Mamun';
+
+var name1 = first1+ " " +last1
+
+console.log (name1);
+
+var sause = 26;
+
+var burger = 78;
+
+var total = sause + burger;
+
+console.log(total);
+
+
+var Noodles = 180;
+
+var Onion = 100;
+
+var total1 = Noodles + Onion;
+
+console.log(total1);
+
+
+var PIE = 3.1416;
+
+var ChickenLegPiece = '60.341';
+
+var LegPiece = parseFloat(ChickenLegPiece);
+
+var ChilliSausage = 34.493;
+
+
+var ChickenTotal = (LegPiece+ChilliSausage);
+
+console.log(ChickenTotal);
+
+
+
+
+var Mango = 234.472;
+
+var Mangoint = parseInt(Mango);
+
+var Strawberry = 300.456;
+
+var Strawberryint = parseInt(Strawberry);
+
+var Fruit = Mangoint + Strawberryint ;
+
+console.log(Fruit);
+
+
+
+
+
+
+
+var budget = 10000;
+
+var people = 100;
+
+var work = budget/people ;
+
+console.log(work);
+
+
+
+
+
+
+
+
+var item1 = 10;
+
+var item2 = 10;
+
+var multiply = item1*item2 ;
+
+console.log(multiply);
+
+
+
+
+
+
+
+var min = 10;
+
+var min1 = 10;
+
+var minus = min - min1;
+
+console.log(minus);
+
+
+
+
+
+
+
+
+var add = 10;
+
+var ad = 10;
+
+var plus = add + ad;
+
+console.log(plus);
+
+
+
+
+
+
+
+var value = 23;
+
+value--
+
+console.log(value);
+
+
+
+
+
+
+var valueAdd = 23;
+
+valueAdd++
+
+console.log(valueAdd);
+
+
+
+
+
+
+
+
+var myName = 'Mamun Seaum';
+myName = 'Seaum Mamun';
+
+console.log(myName);
+
+
+
+
+
+
+
+
+var today = Date();
+
+console.log(today);
+
+
+
+
+
+
+
+var firstNumber = 0.1;
+
+var lastNumber = 0.2;
+
+var totalNumber = firstNumber + lastNumber;
+
+var solve = totalNumber.toFixed(1);
+
+console.log(solve);
+
+
+
+
+
+
+
+
+
+var firstNumber1 = 0.1;
+
+var lastNumber1 = 0.2;
+
+var totalNumber1 = firstNumber1 + lastNumber1;
+
+var solve1 = totalNumber1.toFixed(2);
+
+console.log(solve1);
+
+
+
+
+
+
+
+
+
+
+
+
